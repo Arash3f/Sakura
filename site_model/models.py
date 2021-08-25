1c6880404b5cf161fcf1b24a3165c1732333ccd9
Arash Alfoneh <arash.alfooneh@gmail.com>
2021-08-25 18:28:23 +0430

    change img name

--- site_model/models.py
+++ site_model/models.py
@@ -14,14 +14,15 @@ class Private_Site_Information(models.Model):
     whatsapp_id   = models.CharField(verbose_name="آدرس واتس آپ سایت" ,max_length = 100 , blank=True , null = True)
 
     # part one (img) :
-    product_img_1 = models.ImageField(verbose_name="عکس" ,upload_to = 'main_site/', blank=True , null = True)
-    product_img_2 = models.ImageField(verbose_name="عکس" ,upload_to = 'main_site/', blank=True , null = True)
-    product_img_3 = models.ImageField(verbose_name="عکس" ,upload_to = 'main_site/', blank=True , null = True)
+    product_img_1 = models.ImageField(verbose_name="۱ عکس" ,upload_to = 'main_site/', blank=True , null = True)
+    product_img_2 = models.ImageField(verbose_name="۲ عکس" ,upload_to = 'main_site/', blank=True , null = True)
+    product_img_3 = models.ImageField(verbose_name="۳ عکس" ,upload_to = 'main_site/', blank=True , null = True)
     
     # part one (img) :
-    logo = models.ImageField(verbose_name="عکس" ,upload_to = 'logo/', blank=True , null = True)
-    sign_one = models.ImageField(verbose_name="عکس" ,upload_to = 'sign/', blank=True , null = True)
-    sign_two = models.ImageField(verbose_name="عکس" ,upload_to = 'sign/', blank=True , null = True)
+    logo = models.ImageField(verbose_name="عکس لوگو" ,upload_to = 'logo/', blank=True , null = True)
+    sign_one = models.ImageField(verbose_name="عکس نماد" ,upload_to = 'sign/', blank=True , null = True)
+    sign_two = models.ImageField(verbose_name="عکس نماد " ,upload_to = 'sign/', blank=True , null = True)
+    site_img = models.ImageField(verbose_name="عکس  " ,upload_to = 'site/', blank=True , null = True)
 
     # part one (body) :
     title_1       = models.CharField(verbose_name="سر تیتر" ,max_length = 100 , blank=True , null = True )
